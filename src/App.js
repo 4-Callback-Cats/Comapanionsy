b3247f4a5241f27dd670d3107ac065bcb47e5e91
kartikp36 <kartikp2002@gmail.com>
2021-09-04 16:12:07 +0530

    feat: clone sandbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Navigation from './components/Navigation';
+import About from './components/About';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={About} />
+          {/* <Route path="/home" exact component={Home} /> */}
+          {/* <Route path="/login" exact component={Login} /> */}
+          {/* <Route path="/profile" exact component={Profile} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
