4a20e41869440ad4e08bdb49483a17bcacdbd74b
Rajit <73546886+rajitkuthialamuj@users.noreply.github.com>
2021-09-04 23:58:51 +0530

    Update Card.js

--- src/components/cards-section/Card.js
+++ src/components/cards-section/Card.js
@@ -5,11 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const CardItem = (props) => {
 
   return (
-     
-    <Card style={{ width: '20rem', margin: '2rem'}}>
-      
-        <Card.Header>{props.name}</Card.Header>
-        <Card.Body>
+
+    <Card style={{ width: '20rem', margin: '2rem' }}>
+
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Body>
         <Card.Text>
           {props.text}
         </Card.Text>
