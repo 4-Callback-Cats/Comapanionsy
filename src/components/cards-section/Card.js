2a705eb95583150e1d3d9766101e8ad1b8e8b0b2
kartikp36 <kartikp2002@gmail.com>
2021-09-05 16:28:49 +0530

    fix: conflict at Card.js

--- src/components/cards-section/Card.js
+++ src/components/cards-section/Card.js
@@ -1,35 +1,28 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
+import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const CardItem = (props) => {
-
   return (
-
-    <Card style={{ width: '20rem', margin: '2rem', border: '2px solid #2b2727' }}>
-
+    <Card
+      style={{ width: '20rem', margin: '2rem', border: '2px solid #2b2727' }}
+    >
       <Card.Header>{props.name}</Card.Header>
       <Card.Body>
-        <Card.Text>
-          {props.text}
-        </Card.Text>
-<<<<<<< HEAD
-        <Button style={{ 
-          alignItem: "center",  
-          background: '#DC143C', 
-=======
-        <Button variant="danger" style={{ 
-          alignItem: "center",   
->>>>>>> c80df8450408547464513873585d5e007c3b918d
-          margin: '1rem', 
-          border: '2px' ,
-          }}>Approach</Button>
+        <Card.Text>{props.text}</Card.Text>
+        <Button
+          style={{
+            alignItem: 'center',
+            background: '#DC143C',
+            margin: '1rem',
+            border: '2px',
+          }}
+        >
+          Approach
+        </Button>
       </Card.Body>
     </Card>
-
   );
-
-}
+};
 
 export default CardItem;
-
